b7a26dcbe74bfba232b20ee65ef824fcb343054e
jerielB <jerielbongco98@gmail.com>
2025-06-08 14:09:06 +1000

    Initial commit

---
+++ src/main/java/com/jerielb/additional_stuff/AdditionalStuffClient.java
@@ -0,0 +1,10 @@
+package com.jerielb.additional_stuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdditionalStuffClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
